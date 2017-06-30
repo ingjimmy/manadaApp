41db3669814eb5fb316195855889a86aa21c71bd
ingjimmy <ingjimmy@gmail.comcls>
2017-06-30 11:58:12 -0500

    audio file pipe

--- src/pipes/comment-type/comment-type.ts
+++ src/pipes/comment-type/comment-type.ts
@@ -4,7 +4,19 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'commentType',
 })
 export class CommentTypePipe implements PipeTransform {
-  transform(comments: Array<any>, type: number) {
-    return comments.filter(comment => comment.type === type);
+  transform(comments: Array<any>, type: number) {    
+    if (type == 0) {
+      return comments.filter(comment => comment.type === type);
+    } else if (type == 1) {
+      return comments.filter(comment => {
+         let ext = comment.name.split('.').pop();
+         return comment.type === 1 && ext != 'm4a' && ext != 'wav' && ext != 'mp3';
+      });
+    } else {
+      return comments.filter(comment => {
+         let ext = comment.name.split('.').pop();
+         return comment.type === 1 && (ext == 'm4a' || ext == 'wav' || ext == 'mp3');
+      });
+    }
   }
 }
